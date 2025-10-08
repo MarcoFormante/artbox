4f6f53fab972db382c3d4f4442a87254ae7bfddc
Marco <formante.marco@gmail.com>
2025-10-08 15:07:11 +0200

    add errors list and handle error class

--- ajouter.php
+++ ajouter.php
@@ -1,21 +1,34 @@
 <?php require 'header.php'; ?>
 
+
+<?php //On affiche les erreurs si y en a 
+    if(!empty($errors)): ?>
+    <div id="form-errors-container">
+        <p>Le formulaire contient les erreurs suivantes :</h2>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach ;?>
+        </ul>
+    </div>
+<?php endif ?>
+
 <form action="traitement.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
-        <input required type="text" name="titre" id="titre">
+        <input onchange='titre.classList.remove("input-error")'  class="<?=!empty($errors['titre']) ? 'input-error' : '' ?>"  required type="text" name="titre" id="titre" value=<?=htmlspecialchars($title) ?? ''?>>
     </div>
     <div class="champ-formulaire">
         <label for="artiste">Auteur de l'œuvre</label>
-        <input required type="text" name="artiste" id="artiste">
+        <input onchange='artiste.classList.remove("input-error")'  class="<?=!empty($errors['artiste']) ? 'input-error' : ''?>" required type="text" name="artiste" id="artiste" value=<?=htmlspecialchars($artiste) ?? ''?>>
     </div>
     <div class="champ-formulaire">
         <label for="image">URL de l'image</label>
-        <input required type="url" name="image" id="image">
+        <input onchange='image.classList.remove("input-error")' class="<?=!empty($errors['image']) ? 'input-error' : ''?>" required type="url" name="image" id="image" value=<?=htmlspecialchars($image) ?? ''?>>
     </div>
     <div class="champ-formulaire">
         <label for="description">Description</label>
-        <textarea required name="description" id="description"></textarea>
+        <textarea onchange='description.classList.remove("input-error")' class="<?=!empty($errors['description']) ? 'input-error' : ''?>"  required name="description" id="description"><?=htmlspecialchars($description) ?? ''?></textarea>
     </div>
 
     <input type="submit" value="Valider" name="submit">
