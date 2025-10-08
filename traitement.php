20c3cba60fc4ddff15c6901054aab916473e4f25
Marco <formante.marco@gmail.com>
2025-10-08 15:07:58 +0200

    if errors display errors and form else redirect to Homepage

--- traitement.php
+++ traitement.php
@@ -2,8 +2,6 @@
 require 'functions.php';
 require 'bdd.php';
 
-require 'header.php';
-
 $errors = [];
 $FORM_PARAMS = ['titre', 'artiste', 'image', 'description'];
 
@@ -15,20 +13,20 @@ foreach ($FORM_PARAMS as $param) {
 
     // check if fields are empty
     if(empty($_POST[$param]) || trim($_POST[$param]) === "" ){  
-        $errors[] = "Le champ <b>". strtoupper($param) ."</b> ne peut pas etre vide";
+        $errors[$param] = "Le champ <b>". strtoupper($param) ."</b> ne peut pas etre vide";
     }
 }
 
 
 // check if description field has at least 3 characters
 if (strlen($_POST['description']) < 3) {
-    $errors[] = "Le champ <b> DESCRIPTION </b> doit contenir au moins 3 caractères";
+    $errors['description'] = "Le champ <b> DESCRIPTION </b> doit contenir au moins 3 caractères";
 }
    
 
     // check if is a valid URL  
 if( !filter_var($_POST['image'],FILTER_VALIDATE_URL) || !preg_match('/^https?:\/\/.+\.(jpg|jpeg|png|gif|webp)$/i', $_POST['image']) ) {
-    $errors[] = "Le champ <b> IMAGE </b> doit contenir une URL valide : <br/> <b>Exemple</b>: https://chemin/image.( jpg | jpeg | png | gif )";
+    $errors['image'] = "Le champ <b> IMAGE </b> doit contenir une URL valide : <br/> <b>Exemple</b>: https://chemin/image.( jpg | jpeg | png | gif )";
 }
 
 
@@ -53,21 +51,16 @@ if (!count($errors)) {
         // if error, show a generic error message  
         generateErrorMessage("<b>Une Erreur est survenue, Veuillez retentez plus tard.</b>"); 
     }
+
+}else{
+    $title = htmlspecialchars($_POST['titre']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+    $description = nl2br(htmlspecialchars($_POST['description']));
+    require("ajouter.php");
 }
+
 ?>
 
-<?php if(count($errors)): ?>
-
-<div id="form-errors-container">
-    <p>Le formulaire contient les erreurs suivantes :</h2>
-    <ul>
-        <?php foreach ($errors as $error): ?>
-            <li><?= $error ?></li>
-        <?php endforeach ;?>
-    </ul>
-</div>
-
-<?php endif ?>
 
 
-<?php require 'footer.php'; ?>
