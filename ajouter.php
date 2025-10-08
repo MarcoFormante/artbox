37e3f5f70421a96000e1c6e73d51388ce49364a0
Marco <formante.marco@gmail.com>
2025-10-08 15:15:43 +0200

    update inputs variables

--- ajouter.php
+++ ajouter.php
@@ -16,19 +16,19 @@
 <form action="traitement.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
-        <input onchange='titre.classList.remove("input-error")'  class="<?=!empty($errors['titre']) ? 'input-error' : '' ?>"  required type="text" name="titre" id="titre" value=<?=htmlspecialchars($title) ?? ''?>>
+        <input onchange='titre.classList.remove("input-error")'  class="<?=!empty($errors['titre']) ? 'input-error' : '' ?>"  required type="text" name="titre" id="titre" value=<?=$title ?? "" ?>>
     </div>
     <div class="champ-formulaire">
         <label for="artiste">Auteur de l'œuvre</label>
-        <input onchange='artiste.classList.remove("input-error")'  class="<?=!empty($errors['artiste']) ? 'input-error' : ''?>" required type="text" name="artiste" id="artiste" value=<?=htmlspecialchars($artiste) ?? ''?>>
+        <input onchange='artiste.classList.remove("input-error")'  class="<?=!empty($errors['artiste']) ? 'input-error' : ''?>" required type="text" name="artiste" id="artiste" value=<?=$artiste ?? ""  ?>>
     </div>
     <div class="champ-formulaire">
         <label for="image">URL de l'image</label>
-        <input onchange='image.classList.remove("input-error")' class="<?=!empty($errors['image']) ? 'input-error' : ''?>" required type="url" name="image" id="image" value=<?=htmlspecialchars($image) ?? ''?>>
+        <input onchange='image.classList.remove("input-error")' class="<?=!empty($errors['image']) ? 'input-error' : ''?>" required type="url" name="image" id="image" value=<?= $image ?? ""?>>
     </div>
     <div class="champ-formulaire">
         <label for="description">Description</label>
-        <textarea onchange='description.classList.remove("input-error")' class="<?=!empty($errors['description']) ? 'input-error' : ''?>"  required name="description" id="description"><?=htmlspecialchars($description) ?? ''?></textarea>
+        <textarea onchange='description.classList.remove("input-error")' class="<?=!empty($errors['description']) ? 'input-error' : ''?>"  required name="description" id="description"><?= $description ?? "" ?></textarea>
     </div>
 
     <input type="submit" value="Valider" name="submit">
